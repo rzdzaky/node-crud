8348db02403796e083f1156d63bc50f08d17446c
rzdzaky <76608040+rzdzaky@users.noreply.github.com>
2021-12-05 11:18:37 +0700

    node 2 finish

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mysql = require("mysql");
-
 const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -20,6 +22,17 @@ connection.connect((err) => {
 
 app.get("/", (req, res) => {
 res.render("top.ejs");
+});
+
+app.get("/new", (req, res) => {
+res.render("new.ejs");
+});
+
+app.post("/create", (req, res) => {
+    connection.query("INSERT INTO items (name) VALUES (?)", [req.body.itemName], 
+    (error, results) => {
+        res.redirect("/index");
+    });
 });
 
 app.get("/index", (req, res) => {
